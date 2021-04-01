2d8e6059fb65d55dfff238db11a61a4c0574ba15
Jeff Stevens <jeffreywstevens@hotmail.com>
2021-04-01 15:29:40 -0400

    Works today.  Both ID4 configuration and identities in SQL Server.

---
+++ src/IdentityServer/Startup.cs
@@ -0,0 +1,156 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Linq;
+using System.Reflection;
+
+namespace IdentityServer
+{
+    using IdentityServerAspNetIdentity.Data;
+    using IdentityServerAspNetIdentity.Models;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Configuration;
+
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            const string connectionString = @"Data Source=DESKTOP-MPJ0DR5;database=IdentityServer;trusted_connection=yes;User ID=sa;Password=$@nd@l$";
+
+            var builder = services.AddIdentityServer()
+                //.AddTestUsers(TestUsers.Users)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                }).AddAspNetIdentity<ApplicationUser>();
+            
+
+            builder.AddDeveloperSigningCredential();
+
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    options.ClientId = "<insert here>";
+                    options.ClientSecret = "<insert here>";
+                })
+                .AddOpenIdConnect("oidc", "Demo IdentityServer", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+                    options.SaveTokens = true;
+
+                    options.Authority = "https://demo.identityserver.io/";
+                    options.ClientId = "interactive.confidential";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // this will do the initial DB population
+            //InitializeDatabase(app);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            // using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            // {
+            //     serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+            //
+            //     var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            //     context.Database.Migrate();
+            //     if (!context.Clients.Any())
+            //     {
+            //         foreach (var client in Config.Clients)
+            //         {
+            //             context.Clients.Add(client.ToEntity());
+            //         }
+            //         context.SaveChanges();
+            //     }
+            //
+            //     if (!context.IdentityResources.Any())
+            //     {
+            //         foreach (var resource in Config.IdentityResources)
+            //         {
+            //             context.IdentityResources.Add(resource.ToEntity());
+            //         }
+            //         context.SaveChanges();
+            //     }
+            //
+            //     if (!context.ApiScopes.Any())
+            //     {
+            //         foreach (var resource in Config.ApiScopes)
+            //         {
+            //             context.ApiScopes.Add(resource.ToEntity());
+            //         }
+            //         context.SaveChanges();
+            //     }
+            // }
+        }
+    }
+}
